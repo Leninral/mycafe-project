6bc33cf072c2a9700930d587734c9c63bf1fb585
Pavel Fiadotau <urfeick@gmail.com>
2025-06-01 15:46:27 +0400

    добавил баннер

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const siteModel = [
+    { type: 'banner', src: 'assets/banner2.webp' },
     { type: 'section', title: 'Добро пожаловать', content: 'Лучшее кафе в городе!' },
-    { type: 'footer', text: '© 2025 MyCafe' }
-  ];
+];
 
- 
-  
- 
-  
-  function createSection(title, content) {
+
+function createImage(source) {
+    const img = document.createElement('img');
+    img.src = source;
+    return img;
+}
+
+
+function createSection(title, content) {
     const section = document.createElement('section');
     const h2 = document.createElement('h2');
     h2.textContent = title;
@@ -16,31 +20,24 @@ const siteModel = [
     section.appendChild(h2);
     section.appendChild(p);
     return section;
-  }
-  
-  function createFooter(text) {
-    const footer = document.createElement('footer');
-    footer.textContent = text;
-    return footer;
-  }
+}
 
-  function renderSite(model, container) {
+function renderSite(model, container) {
     container.innerHTML = ''; // очищаем контейнер
     model.forEach(block => {
-      let element;
-      switch(block.type) {
-        case 'section':
-          element = createSection(block.title, block.content);
-          break;
-        case 'footer':
-          element = createFooter(block.text);
-          break;
-        default:
-          element = document.createElement('div');
-          element.textContent = 'Неизвестный блок';
-      }
-      container.appendChild(element);
+        let element;
+        switch (block.type) {
+            case 'section':
+                element = createSection(block.title, block.content);
+                break;
+            case 'banner':
+                element = createImage(block.src);
+            default:
+                element = document.createElement('div');
+                element.textContent = 'Неизвестный блок';
+        }
+        container.appendChild(element);
     });
-  }
-  
-  renderSite(siteModel, document.getElementById('site'));
+}
+
+renderSite(siteModel, document.getElementById('site'));
